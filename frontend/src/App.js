34c02f1f7bf4e8b33e3d92f5699b90b882464d7b
DallasFerguson <icey4343@gmail.com>
2025-07-22 17:14:40 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,14 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { LibraryProvider } from './contexts/LibraryContext';
+import { WishlistProvider } from './contexts/WishlistContext';
 import { NotificationProvider } from './contexts/NotificationContext';
 import { ReviewProvider } from './contexts/ReviewContext';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home/Home';
+import Library from './pages/Library/Library';
+import Wishlist from './pages/Wishlist/Wishlist';
 import GameDetail from './pages/Games/GameDetail/GameDetail';
 import Search from './pages/Games/Search/Search';
 import Trending from './pages/Games/Trending/Trending';
@@ -25,6 +29,9 @@ const router = createBrowserRouter([
       { path: 'search', element: <Search /> },
       { path: 'trending', element: <Trending /> },
       { path: 'games/:id/reviews', element: <Reviews /> },
+      { path: 'library', element: <Library /> },
+      { path: 'wishlist', element: <Wishlist /> },
+      
       // 404
       { path: '*', element: <NotFound /> }
     ]
@@ -40,6 +47,8 @@ function App() {
   return (
     <ThemeProvider>
       <NotificationProvider>
+        <LibraryProvider>
+          <WishlistProvider>
             <ReviewProvider>
               <RouterProvider router={router} />
             </ReviewProvider>
