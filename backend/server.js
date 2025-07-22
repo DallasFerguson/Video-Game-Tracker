7c884d2af7f98860bde4bac600c00df21c845996
DallasFerguson <icey4343@gmail.com>
2025-07-22 16:32:04 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 //middleware
 app.use(cors({
-  origin: 'http://localhost:3000', //my frontend URL
+  origin: 'https://gametracker-frontend-i9yv.onrender.com',
   credentials: true
 }));
 app.use(express.json());
