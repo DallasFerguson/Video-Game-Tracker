2c103dd428e6497f2a428aa3adebf5df65176b79
DallasFerguson <icey4343@gmail.com>
2025-07-22 17:46:13 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,26 @@
-// File: frontend/src/App.js
-// Make sure the import and routes are properly configured
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Import your components (paths may vary)
+import Header from './components/YourHeaderComponent';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import GameDetail from './pages/Games/GameDetail';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { LibraryProvider } from './contexts/LibraryContext';
-import { WishlistProvider } from './contexts/WishlistContext';
-import { NotificationProvider } from './contexts/NotificationContext';
-import { ReviewProvider } from './contexts/ReviewContext';
-import Layout from './components/layout/Layout';
-import Home from './pages/Home/Home';
-import Library from './pages/Library/Library';
-import Wishlist from './pages/Wishlist/Wishlist';
-import GameDetail from './pages/Games/GameDetail/GameDetail';
-import Search from './pages/Games/Search/Search';
-import Trending from './pages/Games/Trending/Trending';
-@@ -25,6 +29,9 @@
-      { path: 'search', element: <Search /> },
-      { path: 'trending', element: <Trending /> },
-      { path: 'games/:id/reviews', element: <Reviews /> },
-      { path: 'library', element: <Library /> },
-      { path: 'wishlist', element: <Wishlist /> },
-      
-      // 404
-      { path: '*', element: <NotFound /> }
-    ]
-@@ -40,14 +47,16 @@
+function App() {
   return (
-    <ThemeProvider>
-      <NotificationProvider>
-        <LibraryProvider>
-          <WishlistProvider>
-            <ReviewProvider>
-              <RouterProvider router={router} />
-            </ReviewProvider>
-          </WishlistProvider>
-        </LibraryProvider>
-      </NotificationProvider>
-    </ThemeProvider>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/game/:id" element={<GameDetail />} />
+            {/* REMOVED: Library, Wishlist, and Review routes */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
