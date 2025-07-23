ea09da96ab1187777717f1ee62af8e89153836a0
DallasFerguson <icey4343@gmail.com>
2025-07-23 15:59:50 -0400

    Update header.jsx

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.css'; 
+import './header.css'; // Update path if needed
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
         <Link to="/" className="logo">GameTracker</Link>
-
-        <nav className="main-nav">
+        
+        <nav className="nav">
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/search">Search</Link></li>
-            {/* No library or wishlist links */}
+            {/* Remove Library and Wishlist links */}
           </ul>
         </nav>
       </div>
