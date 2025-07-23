1a69a8b893425b7d481d395ee651294bc5028153
DallasFerguson <icey4343@gmail.com>
2025-07-23 13:36:10 -0400

    Update header.jsx

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.css';
+import './header.css'; 
 
 const Header = () => {
   return (
@@ -8,14 +8,11 @@ const Header = () => {
       <div className="container">
         <Link to="/" className="logo">GameTracker</Link>
         
-        <nav className="main-nav">
+        <nav className="nav">
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/search">Search</Link></li>
-            {/* Remove these links:
-            <li><Link to="/library">Library</Link></li>
-            <li><Link to="/wishlist">Wishlist</Link></li>
-            */}
+            {/* Remove Library and Wishlist links */}
           </ul>
         </nav>
       </div>
