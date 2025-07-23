7f74f6f4a0d0528103a7af6d5636f283a070a810
DallasFerguson <icey4343@gmail.com>
2025-07-23 13:26:55 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header'; // Import the new Header component
+import Header from './components/Header/header';
 import Home from './pages/Home/Home'; // Adjust path as needed
 import Search from './pages/Search/Search'; // Adjust path as needed
 import GameDetail from './pages/Games/GameDetail/GameDetail'; // Adjust path as needed
