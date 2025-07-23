6b5d7731793af1c2f0afd57eee61386e343c7e55
DallasFerguson <icey4343@gmail.com>
2025-07-23 13:20:32 -0400

    Add files via upload

---
+++ frontend/src/components/Header/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; // You'll need to create this CSS file too
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <Link to="/" className="logo">GameTracker</Link>
+        
+        <nav className="main-nav">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/search">Search</Link></li>
+            {/* No library or wishlist links */}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
